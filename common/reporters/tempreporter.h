6c31aba51352730d5c62bd98733148f3b24e705d
Mikael Bertze <mikael@bertze.com>
2021-01-23 12:48:10 +0100

    Adds support for enabeling/disabling of temperature sensor. The sensor is enabled (if vcc connected to enable pin) before measurement is done

--- common/reporters/tempreporter.h
+++ common/reporters/tempreporter.h
@@ -6,20 +6,28 @@
 class TempReporter : public MqttReporter {
     public:
         //TempReporter(byte pin, mqttConfig mqtt_config, WiFiClient* espClient) 
-        TempReporter(byte pin, String id) 
-          : MqttReporter(), id_(id), oneWire_(pin), tempSensor_(&oneWire_) 
-        {            
+        TempReporter(byte pin, byte enable_pin, String id) 
+          : MqttReporter(), id_(id), enablePin_(enable_pin), oneWire_(pin), tempSensor_(&oneWire_) 
+        {
+          pinMode(enablePin_, OUTPUT);
+          digitalWrite(enable_pin, false);            
         }
 
         void Report() {
+
+          digitalWrite(enablePin_, true);
+          delay(1);
           tempSensor_.requestTemperatures();
           float temp = tempSensor_.getTempCByIndex(0);
+          digitalWrite(enablePin_, false);
           Serial.println(temp);
           report("{ \"id\" : \"" + id_ + "\", \"temp\" : " + String(temp,1) + " }");
+
         }
 
     private:
         String id_;
+        byte enablePin_;
         OneWire oneWire_;
         DallasTemperature tempSensor_; 
 };
